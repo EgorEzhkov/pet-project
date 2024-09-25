540fead4abdae824b5dc19c40ab7f6ae0ffb5811
Egor Ezhkov <egorkashpak21@yandex.ru>
2024-09-25 22:57:18 +0300

    try make a back-slide option

--- frontend/src/components/Carusel/Carusel.tsx
+++ frontend/src/components/Carusel/Carusel.tsx
@@ -13,27 +13,34 @@ const Carusel = () => {
     setArr(testMassiveSongs);
   }, []);
 
-  const nextItem = () => {
+  const nextItem = async () => {
     setBlockButton(true);
-    setSlideItem(4);
-    setTimeout(() => {
-      console.log("timer");
-      setSlideItem(0);
-      const newArr = arr;
-      newArr.push(...newArr.slice(0, 4));
-      for (let i = 0; i < 4; i++) {
-        newArr.shift();
-      }
-      setArr(newArr);
-      setBlockButton(false);
-    }, 500);
+    setSlideItem(visibleItems);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setSlideItem(0);
+    const newArr = arr;
+    newArr.push(...newArr.slice(0, visibleItems));
+    for (let i = 0; i < visibleItems; i++) {
+      newArr.shift();
+    }
+    setArr(newArr);
+    setBlockButton(false);
   };
-  const prevItem = () => {
-    setSlideItem((prev) =>
-      prev === 0 ? testMassiveSongs.length - visibleItems : prev - visibleItems
-    );
+
+  const prevItem = async () => {
+    setBlockButton(true);
+    const newArr = arr;
+    newArr.unshift(...newArr.slice(newArr.length - 4, newArr.length));
+    for (let i = 0; i < visibleItems; i++) {
+      newArr.pop();
+    }
+    setArr(newArr);
+    setSlideItem(-visibleItems);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setSlideItem(0);
+    setBlockButton(false);
   };
-  console.log(arr);
+  console.log(slideItems);
   return (
     <div className={styles.main}>
       <h2 className={styles.title}>Какое-то название</h2>
@@ -43,10 +50,10 @@ const Carusel = () => {
         </button>
         <div
           className={`${styles.slideContainer} ${
-            slideItems === 4 ? styles.transition : null
+            slideItems === visibleItems || slideItems === -visibleItems ? styles.transition : null
           }`}
           style={{
-            transform: `translateX(-${(slideItems * 100) / visibleItems}%)`,
+            transform: `translateX(${(slideItems * -1 * 100) / visibleItems}%)`,
           }}
         >
           {arr.map((el, index) => (
